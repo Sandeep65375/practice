4a10fbc4a143c6267775509bea4ca40d8aa59348
Sandeep65375 <105710743+Sandeep65375@users.noreply.github.com>
2024-05-15 18:50:31 +0000

    addition of three no

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("sandeep");
+// function multiply(num1, num2) {
+//     let result = num1 * num2;
+//     return result;
+//   }
+//   console.log(multiply(2,3)
+
+function addition(num1, num2,num3){
+ let outcome = num1 + num2 + num3;
+ 
+ return outcome;
+
+};
+ console.log(addition(10000023737,16363736,163673737));
